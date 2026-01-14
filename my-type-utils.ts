5628d581986418630ccd5f4c975e336afbdc2a01
Patrick Ren <rz19960718@gmail.com>
2026-01-14 16:08:39 +0800

    Update my-type-utils.ts

--- my-type-utils.ts
+++ my-type-utils.ts
@@ -1,3 +1,11 @@
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {};
+
+// the little `& {}` is a clever hack to trick tsc into fully resolving a type that it might otherwise leave as lazy reference
+// TypeScript is lazy. When you write `[K in keyof T]: T[K]`, TypeScript thinks: "I've mapped the keys of T, but I'll wait until I actually need to check a value against this type before I do the heavy lifting of expanding it."
+// By adding `& {}`, you are introducing a new operation(intersection). TypeScript looks at the mapped type and says: "Wait, I can't just leave this as a reference; I need to intersect it with this empty object now."
+
 /**
  * Utility type to make optional properties compatible with exactOptionalPropertyTypes
  * Auto adds | undefined to properties that are already optional
